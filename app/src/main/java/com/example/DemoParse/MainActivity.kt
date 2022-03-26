91ef3224975a1ca5c0be81b289f46bae9a943360
arty-aj <carturo06@gmail.com>
2022-03-26 12:11:55 -0500

    LogOutButton

--- app/src/main/java/com/example/DemoParse/MainActivity.kt
+++ app/src/main/java/com/example/DemoParse/MainActivity.kt
@@ -54,6 +54,14 @@ class MainActivity : AppCompatActivity() {
             onLaunchCamera()
         }
 
+        findViewById<Button>(R.id.btnLogOut).setOnClickListener{
+            ParseUser.logOut()
+            val currentUser = ParseUser.getCurrentUser() // this will now be null
+            val intent = Intent(this@MainActivity, LoginActivity::class.java)
+            startActivity(intent)
+
+        }
+
         queryPosts()
     }
 
